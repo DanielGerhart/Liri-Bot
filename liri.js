0efcc7d67520497c6318c33a180d27bc6f6da788
Daniel Gerhart <djgerhart@outlook.com>
2019-10-14 20:07:43 -0500

    cleaned up code

--- liri.js
+++ liri.js
@@ -48,174 +48,179 @@ runLiri();
 
 //Switch statement for commands
 function runLiri() {
-    switch (userCommand) {
-        case "concert-this":
+switch (userCommand) {
+    case "concert-this":
+concertThis();
+        break;
+    case "spotify-this-song":
+spotifyThisSong();
+        break;
+    case "movie-this":
+movieThis();
+        break;
+    case "do-what-it-says":
+doWhatItSays();
+        break;
 
-            //Append userInput to log.txt
-            fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
-                if (error) {
-                    console.log(error);
-                };
-            });
-
-            //Run request to bandsintown with the specified artist
-            var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
-            request(queryURL, function (error, response, body) {
-
-                //If no error and response is a success
-                if (!error && response.statusCode === 200) {
-                    //Parse the json response
-                    var data = JSON.parse(body);
-                    //Loop through array
-                    for (var i = 0; i < data.length; i++) {
-                        //Get venue name
-                        console.log("Venue: " + data[i].venue.name);
-                        //Append data to log.txt
-                        fs.appendFileSync("log.txt", "Venue: " + data[i].venue.name + "\n", function (error) {
-                            if (error) {
-                                console.log(error);
-                            };
-                        });
-
-                        //Get venue location
-                        //If statement for concerts without a region
-                        if (data[i].venue.region == "") {
-                            console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
-                            //Append data to log.txt
-                            fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.country + "\n", function (error) {
-                                if (error) {
-                                    console.log(error);
-                                };
-                            });
-
-                        } else {
-                            console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
-                            //Append data to log.txt
-                            fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country + "\n", function (error) {
-                                if (error) {
-                                    console.log(error);
-                                };
-                            });
-                        }
-
-                        //Get date of show
-                        var date = data[i].datetime;
-                        date = moment(date).format("MM/DD/YYYY");
-                        console.log("Date: " + date)
-                        //Append data to log.txt
-                        fs.appendFileSync("log.txt", "Date: " + date + "\n----------------\n", function (error) {
-                            if (error) {
-                                console.log(error);
-                            };
-                        });
-                        console.log("----------------")
-                    }
-                }
-            });
-
-            break;
-        case "spotify-this-song":
-            //If statement for no song provided
-            if (!userInput) {
-                userInput = "The%20Sign";
-                nextUserInput = userInput.replace(/%20/g, " ");
-                console.log("Please input a song!")
-
-            }
-
-            //Append userInput to log.txt
-            fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
-                if (error) {
-                    console.log(error);
-                };
-            });
-
-            // console.log(spotify);
-            spotify.search({
-
-                type: "track",
-                query: userInput
-            }, function (err, data) {
-                if (err) {
-                    console.log("Error occured: " + err)
-                }
-
-                //Assign data being used to a variable
-                var info = data.tracks.items
-                // console.log(info);
-
-                //Loop through all the "items" array
-                for (var i = 0; i < info.length; i++) {
-                    //Store "album" object to variable
-                    var albumObject = info[i].album;
-                    var trackName = info[i].name
-                    var preview = info[i].preview_url
-                    //Store "artists" array to variable
-                    var artistsInfo = albumObject.artists
-                    //Loop through "artists" array
-                    for (var j = 0; j < artistsInfo.length; j++) {
-                        console.log("Artist: " + artistsInfo[j].name)
-                        console.log("Song Name: " + trackName)
-                        console.log("Preview of Song: " + preview)
-                        console.log("Album Name: " + albumObject.name)
-                        console.log("----------------")
-                        //Append data to log.txt
-                        fs.appendFileSync("log.txt", "Artist: " + artistsInfo[j].name + "\nSong Name: " + trackName + "\nPreview of Song: " + preview + "\nAlbum Name: " + albumObject.name + "\n----------------\n", function (error) {
-                            if (error) {
-                                console.log(error);
-                            };
-                        });
-                    }
-                }
-            })
-
-            break;
-        case "movie-this":
-            //If statement for no movie provided
-            if (!userInput) {
-                userInput = "Mr%20Nobody";
-                nextUserInput = userInput.replace(/%20/g, " ");
-            }
-
-            //Append userInput to log.txt
-            fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
-                if (error) {
-                    console.log(error);
-                };
-            });
-
-            //Run request to OMDB
-            var queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
-            request(queryURL, function (error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var info = JSON.parse(body);
-                    console.log("Title: " + info.Title)
-                    console.log("Release Year: " + info.Year)
-                    console.log("OMDB Rating: " + info.Ratings[0].Value)
-                    console.log("Rating: " + info.Ratings[1].Value)
-                    console.log("Country: " + info.Country)
-                    console.log("Language: " + info.Language)
-                    console.log("Plot: " + info.Plot)
-                    console.log("Actors: " + info.Actors)
-
-                    //Append data to log.txt
-                    fs.appendFileSync("log.txt", "Title: " + info.Title + "\nRelease Year: " + info.Year + "\nIMDB Rating: " + info.Ratings[0].Value + "\nRating: " +
-                        info.Ratings[1].Value + "\nCountry: " + info.Country + "\nLanguage: " + info.Language + "\nPlot: " + info.Plot + "\nActors: " + info.Actors + "\n----------------\n",
-                        function (error) {
-                            if (error) {
-                                console.log(error);
-                            };
-                        });
-                }
-            });
-
-            break;
-    }
 }
+function concertThis() {
+    fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
+        if (error) {
+            console.log(error);
+        };
+    });
 
-if (userCommand == "do-what-it-says") {
-    var fs = require("fs");
+    //Run request to bandsintown with the specified artist
+    var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
+    request(queryURL, function (error, response, body) {
 
+        //If no error and response is a success
+        if (!error && response.statusCode === 200) {
+            
+            //Parse the json response
+            var data = JSON.parse(body);
+            
+            //Loop through array
+            for (var i = 0; i < data.length; i++) {
+                //Get venue name
+                console.log("Venue: " + data[i].venue.name);
+                //Append data to log.txt
+                fs.appendFileSync("log.txt", "Venue: " + data[i].venue.name + "\n", function (error) {
+                    if (error) {
+                        console.log(error);
+                    };
+                });
+
+                //Get venue location
+                //If statement for concerts without a region
+                if (data[i].venue.region == "") {
+                    console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+                    //Append data to log.txt
+                    fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.country + "\n", function (error) {
+                        if (error) {
+                            console.log(error);
+                        };
+                    });
+
+                } else {
+                    console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
+                    //Append data to log.txt
+                    fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country + "\n", function (error) {
+                        if (error) {
+                            console.log(error);
+                        };
+                    });
+                }
+
+                //Get date of show
+                var date = data[i].datetime;
+                date = moment(date).format("MM/DD/YYYY");
+                console.log("Date: " + date)
+                //Append data to log.txt
+                fs.appendFileSync("log.txt", "Date: " + date + "\n----------------\n", function (error) {
+                    if (error) {
+                        console.log(error);
+                    };
+                });
+                console.log("----------------")
+            }
+        }
+    });
+}
+function spotifyThisSong() {
+    if (!userInput) {
+        userInput = "The%20Sign";
+        nextUserInput = userInput.replace(/%20/g, " ");
+        console.log("Please input a song!")
+
+    }
+
+    //Append userInput to log.txt
+    fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
+        if (error) {
+            console.log(error);
+        };
+    });
+
+    // console.log(spotify);
+    spotify.search({
+
+        type: "track",
+        query: userInput
+    }, function (err, data) {
+        if (err) {
+            console.log("Error occured: " + err)
+        }
+
+        //Assign data being used to a variable
+        var info = data.tracks.items
+        // console.log(info);
+
+        //Loop through all the "items" array
+        for (var i = 0; i < info.length; i++) {
+            //Store "album" object to variable
+            var albumObject = info[i].album;
+            var trackName = info[i].name
+            var preview = info[i].preview_url
+            //Store "artists" array to variable
+            var artistsInfo = albumObject.artists
+            //Loop through "artists" array
+            for (var j = 0; j < artistsInfo.length; j++) {
+                console.log("Artist: " + artistsInfo[j].name)
+                console.log("Song Name: " + trackName)
+                console.log("Preview of Song: " + preview)
+                console.log("Album Name: " + albumObject.name)
+                console.log("----------------")
+                //Append data to log.txt
+                fs.appendFileSync("log.txt", "Artist: " + artistsInfo[j].name + "\nSong Name: " + trackName + "\nPreview of Song: " + preview + "\nAlbum Name: " + albumObject.name + "\n----------------\n", function (error) {
+                    if (error) {
+                        console.log(error);
+                    };
+                });
+            }
+        }
+    })
+}
+function movieThis() {
+    //If statement for no movie provided
+    if (!userInput) {
+        userInput = "Mr%20Nobody";
+        nextUserInput = userInput.replace(/%20/g, " ");
+    }
+
+    //Append userInput to log.txt
+    fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
+        if (error) {
+            console.log(error);
+        };
+    });
+
+    //Run request to OMDB
+    var queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
+    request(queryURL, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var info = JSON.parse(body);
+            console.log("Title: " + info.Title)
+            console.log("Release Year: " + info.Year)
+            console.log("OMDB Rating: " + info.Ratings[0].Value)
+            console.log("Rating: " + info.Ratings[1].Value)
+            console.log("Country: " + info.Country)
+            console.log("Language: " + info.Language)
+            console.log("Plot: " + info.Plot)
+            console.log("Actors: " + info.Actors)
+
+            //Append data to log.txt
+            fs.appendFileSync("log.txt", "Title: " + info.Title + "\nRelease Year: " + info.Year + "\nIMDB Rating: " + info.Ratings[0].Value + "\nRating: " +
+                info.Ratings[1].Value + "\nCountry: " + info.Country + "\nLanguage: " + info.Language + "\nPlot: " + info.Plot + "\nActors: " + info.Actors + "\n----------------\n",
+                function (error) {
+                    if (error) {
+                        console.log(error);
+                    };
+                });
+        }
+    });
+}
+function doWhatItSays() {
     //Read random.txt file
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
@@ -229,4 +234,5 @@ if (userCommand == "do-what-it-says") {
         nextUserInput = userInput.replace(/%20/g, " ");
         runLiri();
     })
+}
 }
